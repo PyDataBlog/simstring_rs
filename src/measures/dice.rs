8966ba18910da45071dbe2cb715881e3f37fc989
Bernard Brenyah <pimpfada@gmail.com>
2024-07-21 22:38:30 +0200

    WIP: Added Dice measures

--- src/measures/dice.rs
+++ src/measures/dice.rs
@@ -1,4 +1,5 @@
 use super::SimilarityMeasure;
+use std::collections::HashSet;
 
 pub struct Dice;
 
@@ -16,18 +17,25 @@ impl Dice {
 
 impl SimilarityMeasure for Dice {
     fn minimum_feature_size(&self, query_size: i64, alpha: f64) -> i64 {
-        todo!()
+        ((alpha / (2. - alpha)) * query_size as f64).ceil() as i64
     }
 
     fn maximum_feature_size(&self, query_size: i64, alpha: f64) -> i64 {
-        todo!()
+        (((2. - alpha) / alpha) * query_size as f64).floor() as i64
     }
 
     fn similarity_score(&self, x: &Vec<i64>, y: &Vec<i64>) -> f64 {
-        todo!()
+        let set_x: HashSet<_> = x.iter().collect();
+        let set_y: HashSet<_> = y.iter().collect();
+
+        let intersection_count = set_x.intersection(&set_y).count() as f64;
+
+        let denominator = (set_x.len() + set_y.len()) as f64;
+
+        2. * (intersection_count / denominator)
     }
 
     fn minimum_overlap(&self, query_size: i64, candidate_size: i64, alpha: f64) -> i64 {
-        todo!()
+        (0.5 * alpha * query_size as f64 * candidate_size as f64).ceil() as i64
     }
 }

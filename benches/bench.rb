2c95f536a91daa557c1fd167b1e1d5086010ccb6
Bernard Brenyah <pimpfada@gmail.com>
2024-12-22 11:30:38 +0100

    CI: Updated ruby benchmarks for search

--- benches/bench.rb
+++ benches/bench.rb
@@ -6,21 +6,23 @@ def create_db(ngram_size)
   SimString::Database.new(SimString::NGramBuilder.new(ngram_size))
 end
 
-def bench_insert
+def load_companies
   filepath = File.join(Dir.pwd, "benches", "data", "company_names.txt")
-  companies = File.readlines(filepath, chomp: true)
+  File.readlines(filepath, chomp: true)
+end
 
-  puts "Found #{companies.size} companies"
-  puts "\nBenchmark Results:"
+def bench_insert
+  companies = load_companies()
+
+  puts "\nBenchmarking database insertions:"
   puts "-" * 40
 
-  # Test for n-gram sizes 2, 3, and 4
   [2, 3, 4].each do |ngram_size|
     measurements = []
     start_time = Time.now
     iterations = 0
 
-    while Time.now - start_time < 40 && iterations < 100
+    while Time.now - start_time < 20 && iterations < 100
       time = Benchmark.realtime do
         db = create_db(ngram_size)
         companies.each do |company|
@@ -38,4 +40,43 @@ def bench_insert
   end
 end
 
-bench_insert
+def bench_search
+  companies = load_companies()
+  search_terms = companies[0...100]  # Use first 100 companies as search terms
+
+  puts "\nBenchmarking database searches:"
+  puts "-" * 40
+
+  [2, 3, 4].each do |ngram_size|
+    db = create_db(ngram_size)
+    companies.each { |company| db.add(company) }
+
+    [0.6, 0.7, 0.8].each do |threshold|
+      measurements = []
+      start_time = Time.now
+      iterations = 0
+
+      while Time.now - start_time < 20 && iterations < 100
+        time = Benchmark.realtime do
+          search_terms.each do |term|
+            db.search(term, threshold)
+          end
+        end
+        measurements << time
+        iterations += 1
+      end
+
+      puts "ngram_#{ngram_size} (threshold=#{threshold}):"
+      puts "  Mean: #{(measurements.mean * 1000).round(2)}ms"
+      puts "  Std Dev: #{(measurements.standard_deviation * 1000).round(2)}ms"
+      puts "  Iterations: #{measurements.length}"
+    end
+  end
+end
+
+def main
+  bench_insert
+  bench_search
+end
+
+main

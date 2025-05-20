19b2e9fabbab4b6866e01504557f5199b34544f3
Bernard Brenyah <pimpfdad@gmail.com>
2025-05-20 22:56:27 +0200

    WIP: clean slate

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod database;
-pub mod extractors;
-pub mod measures;
-pub mod search;
-
-pub use crate::database::{HashDB, SimStringDB};
-pub use crate::extractors::{CharacterNGrams, FeatureExtractor, WordNGrams};
-pub use crate::measures::{Cosine, Dice, ExactMatch, Jaccard, Overlap, SimilarityMeasure};
--- src/search.rs
+++
@@ -1,4 +0,0 @@
-pub struct SearchResult<T> {
-    pub value: T,
-    pub score: f64,
-}
